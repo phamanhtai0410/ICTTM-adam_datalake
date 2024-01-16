4bdf0a2df1191885df10290cf8ae93fb126cf799
Long Phan <long@icttm.net>
2024-01-16 11:55:18 +0700

    - Adding chennai RoC - Update print_progress changing display UI

--- crawlers/india/crawler_india_chennai_companies.py
+++ crawlers/india/crawler_india_chennai_companies.py
@@ -7,11 +7,11 @@ from engines.crawler_engine import crawler
 def main():
     # Create a CloudScraper instance
     scraper = cloudscraper.create_scraper()
-    file_path = 'data/india_mumbai_roc_ageF.txt'  # File path to save the elements
+    file_path = 'data/india_chennai.txt'  # File path to save the elements
     max_retries_per_page = 5  # Maximum number of retries for a page
     sleep_duration_on_success = random.uniform(0, 0.2)  # Duration to sleep after each successful response (in seconds)
-    total_pages = 3516  # Total number of pages to scrape
-    url_base = f"https://www.zaubacorp.com/company-list/age-F/roc-RoC-Mumbai/p-"
+    total_pages = 5109  # Total number of pages to scrape
+    url_base = f"https://www.zaubacorp.com/company-list/roc-RoC-Chennai/p-"
     page_start = 1
     crawler(file_path, total_pages, max_retries_per_page, scraper, sleep_duration_on_success, url_base, page_start)
 
