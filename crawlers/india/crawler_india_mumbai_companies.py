50c404e3d729085117eb1f8d16265ab50e831be6
phuoc-icttm <phuoc@icttm.net>
2024-01-16 15:45:29 +0700

    Revert "- Adding chennai RoC"
    This reverts commit 4bdf0a2df1191885df10290cf8ae93fb126cf799.

--- crawlers/india/crawler_india_mumbai_companies.py
+++ crawlers/india/crawler_india_mumbai_companies.py
@@ -10,7 +10,7 @@ def main():
     file_path = 'data/india_mumbai_roc_ageF.txt'  # File path to save the elements
     max_retries_per_page = 5  # Maximum number of retries for a page
     sleep_duration_on_success = random.uniform(0, 0.2)  # Duration to sleep after each successful response (in seconds)
-    total_pages = 3516  # Total number of pages to scrape
+    total_pages = 5921  # Total number of pages to scrape
     url_base = f"https://www.zaubacorp.com/company-list/age-F/roc-RoC-Mumbai/p-"
     page_start = 1
     crawler(file_path, total_pages, max_retries_per_page, scraper, sleep_duration_on_success, url_base, page_start)
