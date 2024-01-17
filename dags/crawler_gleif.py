b9238bf8caa6cbd1676447326f5507fbf59910b3
Q.T.N <quangtn24198@gmail.com>
2024-01-17 16:06:02 +0700

    Crawl gleif, latvia

--- dags/crawler_gleif.py
+++ dags/crawler_gleif.py
@@ -6,6 +6,14 @@ from zipfile import ZipFile
 import time
 import datetime
 from tqdm import tqdm
+import boto3
+from airflow.models import Variable
+from airflow.exceptions import AirflowException
+
+object_storage_access_key = Variable.get("objs3_access_key", "demo-access-key")
+object_storage_secret_key = Variable.get("objs3_secret_key", 'demo-secret-key')
+object_storage_endpoint = Variable.get("objs3_endpoint", "http://172.17.0.1:9000")
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -85,6 +93,19 @@ def extract_zip_file():
     os.remove(file_path)
     return new_file_name
 
+def upload_file_to_minio(file_path, minio_bucket, minio_object_name):
+    try:
+        s3c = boto3.resource('s3',
+                            endpoint_url=object_storage_endpoint,
+                            aws_access_key_id=object_storage_access_key,
+                            aws_secret_access_key=object_storage_secret_key,
+                            config=boto3.session.Config(signature_version='s3v4'),
+                            verify=False
+                            )
+        s3c.Bucket(minio_bucket).upload_file(file_path, minio_object_name)
+    except Exception as e:
+        raise AirflowException(f"Failed to upload file to Minio: {str(e)}")
+    
 with DAG(
         dag_id='gleif_crawler',
         start_date=datetime.datetime(2023, 12, 23),
@@ -95,6 +116,15 @@ with DAG(
         task_id='download_file',
         python_callable=extract_zip_file,
     )
+    upload_task = PythonOperator(
+        task_id='upload_file',
+        python_callable=upload_file_to_minio,
+        op_kwargs={'file_path': os.path.join(current_dir, 'data-gleif', 'data.csv'), 
+                   'minio_bucket': 'warehouse',
+                   'minio_object_name': 'files/gleif.file/data.csv'}
+    )
+
+    download_task >> upload_task  # Set dependencies
     
 if __name__ == "__main__":
     dag.test()
