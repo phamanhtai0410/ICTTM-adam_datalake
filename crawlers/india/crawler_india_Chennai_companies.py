4d3d05fbbf0997e4d78886173e04b4f7460f1817
Long Phan <long@icttm.net>
2024-01-22 12:59:57 +0700

    Update data as task

--- crawlers/india/crawler_india_Chennai_companies.py
+++ crawlers/india/crawler_india_Chennai_companies.py
@@ -7,12 +7,12 @@ from engines.crawler_engine import crawler
 def main():
     # Create a CloudScraper instance
     scraper = cloudscraper.create_scraper()
-    file_path = 'url/india_.txt'  # File path to save the elements
+    file_path = 'url/india_chennai.txt'  # File path to save the elements
     max_retries_per_page = 5  # Maximum number of retries for a page
     sleep_duration_on_success = random.uniform(0, 0.2)  # Duration to sleep after each successful response (in seconds)
-    total_pages = 5920  # Total number of pages to scrape
+    total_pages = 5109  # Total number of pages to scrape
     page_start = 1
-    url = f"https://www.zaubacorp.com/company-list/roc-RoC-Kanpur/p-"
+    url = f"https://www.zaubacorp.com/company-list/roc-RoC-Chennai/p-"
     crawler(file_path, total_pages, max_retries_per_page, scraper, sleep_duration_on_success, url, page_start)
 
 
